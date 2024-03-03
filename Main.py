15da0cb52b295729c405acc46ccc0fe78948a4f9
Coldsnipe_7902 <145302711+Colsnipe7902@users.noreply.github.com>
2024-03-03 12:09:36 -0500

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,27 @@
+# Starter code starts here
+
+def display_welcome_banner():
+    welcome_banner = """
+                    Welcome to Five To Win!
+
+        This is a dice game between two players, the user
+        and a computer opponent. The game is played in rounds. 
+
+        In each round: 
+
+        - Player rolls two dice and check parity.
+
+        - Parity is whether a number is even or odd.
+
+        - If the parity of both the dice rolls is even, the player gets one point.
+
+        - Else if the parity of both the dice rolls is odd, the computer gets one point. 
+
+        - The game continues until a player reaches 5 or more points. 
+    """
+
+    print(welcome_banner)
+
+display_welcome_banner()
+
+# Starter code ends here
